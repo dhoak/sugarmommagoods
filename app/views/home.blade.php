41ce2eecdcf2f71541e1e9d67275f1bdad3e9de7
David Hoak <davidhoak@Davids-MacBook-Pro.local>
2014-10-15 12:56:03 -0400

    Initial Commit

---
+++ app/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+
+@section('content')
+<p>Hello World.</p>
+
+<section>
+This is section 1
+</section>
+
+<section>
+    This is section 2
+</section>
+
+<section>
+    This is section 3
+</section>
+
+<section>
+    This is section 4
+</section>
+
+<section>
+    This is section 5
+</section>
+
+<section>
+    This is section 6 and it's the best
+</section>
+
+
+@stop
